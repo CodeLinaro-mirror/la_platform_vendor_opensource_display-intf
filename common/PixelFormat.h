4b9298db3a42844fe47970152ba5a8a08de1867f
Dheepthi S <dheepthi@qti.qualcomm.com>
2026-01-16 14:39:10 +0530

    display-intf: Add TP10 UBWC flex formats
    Add TP10 UBWC flex formats for 2,4,8 batch modes
    
    Change-Id: I7c6f3564faafbb2071b670e7a0be47c8a062efe5

--- common/PixelFormat.h
+++ common/PixelFormat.h
@@ -375,6 +375,11 @@ typedef enum vendor_qti_hardware_display_common_PixelFormat : uint32_t {
   YCBCR_P010_HEIF = 0x151, // YCBCR_P010_512
   YCBCR_P010_1024 = 0x152,
   NV12_1024 = 0x153,
+
+  TP10_UBWC_FLEX = 0x154,
+  TP10_UBWC_FLEX_2_BATCH = 0x155,
+  TP10_UBWC_FLEX_4_BATCH = 0x156,
+  TP10_UBWC_FLEX_8_BATCH = 0x157,
   /* --------------------------------------------------------------------------------*/
 
 } vendor_qti_hardware_display_common_PixelFormat;
